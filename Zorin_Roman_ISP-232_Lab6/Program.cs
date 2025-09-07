dc7740170ea6004e7c263a1e439eb0faeb5e3bed
Роман Зорин <sgena170315@gmail.com>
2025-09-07 13:15:10 +0300

    Add project files.

---
+++ Zorin_Roman_ISP-232_Lab6/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Lab5
+{
+    class Lab5
+    {
+        static void Main()
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine(y);
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок Finally");
+            }
+            Console.WriteLine("Конец программы.");
+            
+        }
+    }
+}
