d213a753febff500f1be592dd15bc24acee765c0
Роман Зорин <sgena170315@gmail.com>
2025-09-07 13:25:18 +0300

    Задание 1. Расширение функциональности калькулятора

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,101 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("Введите первое число: ");
+                    double number1 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число: ");
+                    double number2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите операцию (+, -, *, /, ^): ");
+                    string? op = Console.ReadLine();
+
+                    double res = 0;
+                    bool validOperation = true;
+
+                    if (op == "+") res = Add(number1, number2);
+                    else if (op == "-") res = Subtract(number1, number2);
+                    else if (op == "*") res = Multiply(number1, number2);
+                    else if (op == "/")
+                    {
+                        if (number2 == 0)
+                            throw new DivideByZeroException("Ошибка! деление на ноль");
+                        res = Divide(number1, number2);
+                    }
+                    else if (op == "^") res = Pow(number1, number2);
+                    else
+                    {
+                        Console.WriteLine("Неверная операция!");
+                        validOperation = false;
+                    }
+
+                    if (validOperation)
+                    {
+                        Console.WriteLine($"Результат: {number1} {op} {number2} = {res}");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка ввода! Введите корректные числа.");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Ошибка ввода. {ex.Message}.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка непредвиденная ошибка. {ex.Message}.");
+                }
+
+                Console.WriteLine("Введите 'exit' для выхода. \nНажмите любую клавишу для продолжения");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "exit")
+                {
+                    flag = false;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+            Console.WriteLine("Работа калькулятора завершена. Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+
+        static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Divide(double a, double b)
+        {
+            return a / b;
+        }
+
+        static double Pow(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
