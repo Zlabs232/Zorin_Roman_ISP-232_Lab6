51ef4ca40f893775bff8b05bc4f8a31ea7faca32
Роман Зорин <sgena170315@gmail.com>
2025-09-07 13:17:20 +0300

    Был добавлен консольный калькулятор

--- Zorin_Roman_ISP-232_Lab6/Program.cs
+++ Zorin_Roman_ISP-232_Lab6/Program.cs
@@ -20,7 +20,53 @@ namespace Lab5
                 Console.WriteLine("Блок Finally");
             }
             Console.WriteLine("Конец программы.");
-            
+
+            while (true)
+            {
+                Console.WriteLine("Введите первое число: ");
+                double number1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите первое число: ");
+                double number2 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите операцию (+,-,*,/): ");
+                string? op = Console.ReadLine();
+
+                double res = 0;
+
+                try
+                {
+                    if (op == "+") res = number1 + number2;
+                    else if (op == "-") res = number1 - number2;
+                    else if (op == "*") res = number1 * number2;
+                    else if (op == "/")
+                    {
+                        if (number2 == 0)
+                            throw new DivideByZeroException("Ошибка! деление на ноль");
+                        res = number1 / number2;
+                    }
+
+                    else Console.WriteLine("Неверная операция!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка ввода! Введите корректные числа.");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine($"Ошибка ввода. {ex.Message}.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка непредвиденная ошибка . {ex.Message}.");
+                }
+                finally
+                {
+                    Console.WriteLine("Работа калькулятора завершна.");
+                }
+            }
+
         }
     }
 }
+
